f26e0735585a8acc02e2c63f924cf891407443d0
Igor Silva <158208997+IgorPMSilva@users.noreply.github.com>
2024-09-12 13:52:52 -0300

    Commit inicial

---
+++ src/js/painel.js
@@ -0,0 +1,35 @@
+
+
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+setaAvancar.addEventListener("click", function() {
+    if (imagemAtual === imagensPainel.length - 1) {
+        return;
+    }
+    imagemAtual++;
+    imagemAtual++;
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar');
+    });
+    imagensPainel[imagemAtual].classList.add('mostrar');
+});
+    
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    
+    imagensPainel[imagemAtual].classList.add('mostrar');
+});
+
+setaVoltar.addEventListener("click", function() {
+    if(imagemAtual === 0) {
+        return;
+    }
+    imagemAtual--;
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+    imagensPainel[imagemAtual].classList.add('mostrar');
+})
